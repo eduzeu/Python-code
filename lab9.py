045737d686c5deb9b81eea1e366d581cf533f924
Eduardo Hernandez <112764648+eduzeu@users.noreply.github.com>
2023-09-30 19:25:47 -0400

    Add files via upload

---
+++ lab9.py
@@ -0,0 +1,10 @@
+def mult(c,n):
+    
+    '''uses only a loop  and addition to multiply c by the integer n.'''
+    counter = 0
+    for index in range(n):
+        counter = counter + c 
+    return counter
+
+
+ 
